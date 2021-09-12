d1875251c62b3159af7371432d1fbec71832b237
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-12 17:26:40 +0000

    added count  down timer component

---
+++ src/components/Timer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Timer() {
+    return (
+        <div className="timer font-big">
+            <span className="mr-2">02d</span>
+            <span>02h</span> :
+            <span>25m</span> :
+            <span>04s</span>
+        </div>
+    )
+}
+
+export default Timer

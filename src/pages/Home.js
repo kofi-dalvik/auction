42ce76856294e01deb8de9b3151e2458bfe21846
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-12 17:26:53 +0000

    added  items  listingsto home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,46 @@
+import {useState, useContext, useEffect} from 'react';
+import { StateContext, DispatchContext} from './../store';
 import AuthLayout from './AuthLayout';
+import { SET_ITEMS } from '../store/action-types';
 import ItemList from '../components/ItemList';
+import ItemFilters from '../components/ItemFilters';
+import Pagination from '../components/Pagination';
+import {index as getItems} from './../adapters/items';
 
 function Home() {
-    const items = [0,1,2,3,4,5,6,7,8,9];
+    const { items } = useContext(StateContext);
+    const dispatch = useContext(DispatchContext);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [params, setParams] = useState({sortPrice: 'any', keyword: ''})
+
+    useEffect(() => {
+        getItems({ ...params, page: currentPage })
+            .then(response => {
+                console.log(response)
+                dispatch({
+                    type: SET_ITEMS,
+                    payload: response
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }, [params, currentPage, dispatch])
+
     return (
         <AuthLayout className="pt-3">
-            <ItemList items={items} />
+            <ItemFilters
+                setParams={setParams}
+            />
+
+            <ItemList
+                items={items.all}
+            />
+
+            <Pagination
+                pageDetails={items.pageDetails}
+                setCurrentPage={setCurrentPage}
+            />
         </AuthLayout>
     )
 }

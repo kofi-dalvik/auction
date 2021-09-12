05dee1acb4ff92e26cdd4f82f3664a5514d17b38
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-12 03:08:59 +0000

    added login page UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,31 @@
 function Login() {
     return (
-        <h1>
-            Login
-        </h1>
+        <div className="row justify-content-center mt-5">
+            <div className="col-lg-4 col-md-6 col-sm-8 col-12">
+                <div className="login-page elevation rounded">
+                    <div className="text-center mb-5 title">
+                        <h1>Scopic Test</h1>
+                        <hr/>
+                        <h4>Login to Your Account</h4>
+                        <p className="m-0">Start bidding for free</p>
+                    </div>
+
+                    <form className="p-3 pr-5 pl-5">
+                        <div className="form-group elevation mb-4">
+                            <label htmlFor="username" className="form-label">Username</label>
+                            <input type="text" className="form-control" id="username" placeholder=".e.g johndoe"/>
+                        </div>
+
+                        <div className="form-group elevation mb-3">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input type="password" className="form-control" id="password" placeholder=".e.g secret" />
+                        </div>
+
+                        <button type="submit" className="btn btn-primary btn-block mt-5 font-small">Login to Your Account</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     )
 }
 

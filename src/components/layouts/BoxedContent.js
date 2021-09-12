6185441dc184364d14b52a4ad07c310fd3d88db2
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-12 10:32:02 +0000

    added wrapper for boxed content

---
+++ src/components/layouts/BoxedContent.js
@@ -0,0 +1,13 @@
+function BoxedContent({className, children}) {
+    return (
+        <div className="row justify-content-center v-stretch">
+            <div className="col-lg-8 col-md-10 col-sm-12 col-12 v-stretch">
+                <div className={className}>
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BoxedContent

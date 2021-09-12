8fe7d9d618d082e749c487bc0832501930d97be1
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-12 17:27:20 +0000

    added  enhancements

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import Item from './Item';
-import Pagination from './Pagination';
 
 function ItemList({ items }) {
     return (
         <>
             <div className="row">
                 {items.map((item, index) => (
-                    <div className="col-lg-4 col-md-6 col-sm-12 col-12" key={index}>
+                    <div className="col-lg-4 col-md-6 col-sm-12 col-12 v-stretch mb-4" key={index}>
                         <Item item={item}/>
                     </div>
                 ))}
             </div>
-
-            <Pagination />
         </>
     )
 }
--- src/store/action-types.js
+++ src/store/action-types.js
@@ -1,1 +1,3 @@
 export const SET_AUTH_USER = "SET_AUTH_USER";
+
+export const SET_ITEMS = "SET_ITEMS";

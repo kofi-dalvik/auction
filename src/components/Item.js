6ec550e0104e734fb74958b618380834324d8879
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-12 17:25:59 +0000

    added enhancement

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,45 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import Carousel from './Carousel';
+import CurrentBidder from './CurrentBidder';
+import Timer from './Timer';
+import Carousel from './Carousel';
 
 function Item({item}) {
     // const images = ['/logo512.png', '/logo512.png', '/logo512.png',];
 
     return (
-        <div className="auction-item mb-4 elevation">
+        <div className="auction-item elevation v-stretch">
             <div className="media">
                 <div className="actions">
                     <button className="button elevations font-weight-bold">
-                        $ 100
+                        $ { item.price }
                     </button>
 
-                    <Link to={`items/${item}`} className="button elevation font-xs bid-now">
+                    <Link to={`items/${item.id}`} className="button elevation font-xs bid-now">
                         Bid Now
                     </Link>
                 </div>
 
-                {/* <Carousel images={images} /> */}
+                <Carousel images={item.images} />
 
-                <div className="timer">
-                    <span className="mr-2">02d</span>
-                    <span>02h</span> :
-                    <span>25m</span> :
-                    <span>04s</span>
-                </div>
+                <Timer />
             </div>
 
             <div className="description mt-1 p-2">
-                <h5 className="text-white font-big">Blue and Pink Abstract</h5>
-                <p className="text-muted">Add captions to your slides easily with the .carousel-caption element within any .carousel-item. They can be easily hidden on smaller...</p>
+                <h5 className="text-white font-small text-truncate">{item.name}</h5>
+                <p className="mb-1 text-muted text-truncate">{item.description}</p>
 
-                <p className="text-secondary mb-1">Current Bid</p>
-                <div className="current-bidder">
-                    <img src="/logo192.png" alt="current bidder" className="mr-2"/>
-                    <div className="details">
-                        <p className="m-0 text-muted">John Doe</p>
-                        <p className="m-0 font-big text-white">$120</p>
-                    </div>
-                </div>
+                <CurrentBidder bid={item.latest_bid} />
             </div>
         </div>
     )

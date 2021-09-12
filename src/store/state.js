ec1136be9aab62c6d262e1af7f974396b4f80417
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-12 17:27:11 +0000

    added items

--- src/store/state.js
+++ src/store/state.js
@@ -2,13 +2,25 @@ import { storageKeys } from './actions';
 
 //Get stored user & token from localstorage
 const user = JSON.parse(localStorage.getItem(storageKeys.user));
-const token = JSON.parse(localStorage.getItem(storageKeys.token));
+const token = localStorage.getItem(storageKeys.token);
 
 const state = {
     auth: {
         user,
         token,
         isLoggedIn: !!user
+    },
+
+    items: {
+        all: [],
+        pageDetails: {
+            to: 0,
+            from: 0,
+            total: 0,
+            perPage: 0,
+            lastPage: 0,
+            currentPage: 0,
+        }
     }
 }
 

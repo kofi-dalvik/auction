40a3186772e4d19d3a8a4b57bcf47f3ba124902a
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-12 17:25:35 +0000

    added carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,27 +1,19 @@
 import React from 'react'
 
-const carouselId = `carousel-${Math.random().toString(16).split('.').pop()}`;
-
 function Carousel({images}) {
+    const slides = images.map((slide, index) => (
+        <div
+            className={`carousel-item ${index === 0 ? 'active' : ''}`}
+            key={index}>
+            <img src={slide.image_url} className="d-block w-100" alt="..." />
+        </div>
+    ));
+
     return (
-        <div id={carouselId} className="carousel slide" data-ride="carousel">
+        <div className="carousel slide" data-ride="carousel">
             <div className="carousel-inner">
-                {  images.map((url, index) =>  (
-                    <div className="carousel-item" key={index}>
-                        <img className="d-block w-100" src={url} alt={`Slide ${index}`} />
-                    </div>
-                )) }
+                {slides}
             </div>
-
-            <a className="carousel-control-prev" href={`#${carouselId}`} role="button" data-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="sr-only">Previous</span>
-            </a>
-
-            <a className="carousel-control-next" href={`#${carouselId}`} role="button" data-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="sr-only">Next</span>
-            </a>
         </div>
     )
 }

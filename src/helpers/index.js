0a45f78f5b6dcfbc2cde02cb39d42cef400abc56
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-12 22:47:21 +0000

    added sweet  alert  helpers

---
+++ src/helpers/index.js
@@ -0,0 +1,22 @@
+import Swal from 'sweetalert2';
+
+export const notifyError = (text, title = `Error`, okay = 'Okay') => {
+    return Swal.fire({
+        title,
+        text,
+        icon: 'error',
+        confirmButtonText: okay
+    })
+}
+
+export const showConfirm = (title, text, okay = `Confirm`) => {
+    return Swal.fire({
+        title,
+        text,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: okay
+    })
+}

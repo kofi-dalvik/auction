dc27fc81f9c4c46055751184dd60db2311d77568
Ahmed Dawuda <kofidahmed@gmail.com>
2021-09-13 13:08:18 +0000

    added export for all components

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+export {default as Item } from "./Item";
+export {default as Timer } from "./Timer";
+export { default as Button } from "./Button";
+export {default as BidNow } from "./BidNow";
+export {default as Carousel } from "./Carousel";
+export {default as ItemList } from "./ItemList";
+export {default as BidConfig } from "./BidConfig";
+export {default as Pagination } from "./Pagination";
+export {default as ItemFilters } from "./ItemFilters";
+export {default as CurrentBidder } from "./CurrentBidder";
+export {default as ToggleAutoBid } from "./ToggleAutoBid";
+export {default as BiddingHistory } from "./BiddingHistory";
+
+
